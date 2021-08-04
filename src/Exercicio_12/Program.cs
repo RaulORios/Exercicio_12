a143d67d5026f61332b99efa58c48970661589ab
RaulORios <raul.orios89@outlook.com>
2021-08-04 17:57:06 -0300

    Program.cs adicionado

---
+++ src/Exercicio_12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ToDo - Alterar a variável INT para Decimal. 
+
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*********     Exercicio 12     *********");
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine("");
+
+            int temperatura;
+
+            Console.Write("Informe a temperatura em FAHRENHEIT: ");
+            temperatura = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"A temperatura de {temperatura}ºF em Graus Celsius é: {(temperatura - 32) / 1.8}ºC ");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
