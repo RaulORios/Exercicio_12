79d7da6c2f1245bbfa13a08781ea3058a4068c72
RaulORios <raul.orios89@outlook.com>
2021-08-04 18:05:13 -0300

    Alterado variavel int para double

--- src/Exercicio_12/Program.cs
+++ src/Exercicio_12/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercicio_12
 {
@@ -6,22 +7,23 @@ namespace Exercicio_12
     {
         static void Main(string[] args)
         {
-            //ToDo - Alterar a variável INT para Decimal. 
-
+            
             Console.WriteLine("****************************************");
             Console.WriteLine("*********     Exercicio 12     *********");
             Console.WriteLine("****************************************");
 
             Console.WriteLine("");
 
-            int temperatura;
+            double temperatura;
 
             Console.Write("Informe a temperatura em FAHRENHEIT: ");
-            temperatura = Convert.ToInt32(Console.ReadLine());
+            temperatura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double conversao = (temperatura - 32) / 1.8;
 
             Console.WriteLine("");
 
-            Console.WriteLine($"A temperatura de {temperatura}ºF em Graus Celsius é: {(temperatura - 32) / 1.8}ºC ");
+            Console.WriteLine($"A temperatura de {temperatura.ToString("F1",CultureInfo.InvariantCulture)}ºF em Graus Celsius é: {conversao.ToString("F1",CultureInfo.InvariantCulture)}ºC ");
 
             Console.ReadKey();
 
